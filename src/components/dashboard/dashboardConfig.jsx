8601cb3189073215a426d413ed019b8c15cef445
emersonpddp <emersonpddp@gmail.com>
2022-11-27 22:44:38 -0300

    Transição de paginas não funciona

--- src/components/dashboard/dashboardConfig.jsx
+++ src/components/dashboard/dashboardConfig.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import ButtonSwitch from './buttonSwitch'
-import Navbar from './navbar'
 import PanelHeader from './panelHeader'
 import PanelLine from './panelLine'
 import Sidebar from './sidebar'
+import Navbar from './navbar'
+import '../../css/dashboardConfig.css'
 
 const DashboardConfig = () => {
   return (
     <>
-      <Navbar/>
-      <Sidebar/>
-      <PanelHeader p1={'Configurações gerais'} h1={'Sobre'} h5={'Status'}/>
-      <PanelLine h1={'Reservas'} h5={<ButtonSwitch/>}/>
+      <Navbar />
+      <Sidebar />
+      <PanelHeader p1={'Configurações gerais'} h1={'Sobre'} h5={'Status'} />
+      <PanelLine h1={'Reservas'} h5={<ButtonSwitch />} />
     </>
   )
 }
